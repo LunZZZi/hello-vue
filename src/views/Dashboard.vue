be15f09e7c3c47533fac9de212b9a310cd32858d
Bohao Wang <bj050323@gmail.com>
2020-09-01 21:14:39 +0800

    add: todolist

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,14 +2,17 @@
   <div class="dashboard">
     <h1>hello {{ username }}</h1>
     <router-link :to="path">Your Profile</router-link>
+    <Todoist />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Todoist from "@/components/Todoist";
 
 export default {
   name: 'Dashboard',
+  components: {Todoist},
   computed: {
     username() {
       return this.$route.params.username
